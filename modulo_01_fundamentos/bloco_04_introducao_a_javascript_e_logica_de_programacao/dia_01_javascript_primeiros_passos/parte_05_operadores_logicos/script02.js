f04b47639ec9d0596db3d83a2e3db9ebc376bc3f
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-06-14 16:11:52 -0300

    Cria estrutura switch case que atribui um dia da semana à weekDay dependendo do valor aleatório de randomNumber

--- modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_05_operadores_logicos/script02.js
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_05_operadores_logicos/script02.js
@@ -1,4 +1,29 @@
-let weekDay = 'quarta-feira';
+let weekDay = '';
+
+const randomNumber = Math.floor(Math.random() * 7) + 1;
+
+switch (randomNumber) {
+  case 1:
+    weekDay = 'domingo';
+    break;
+  case 2:
+    weekDay = 'segunda-feira';
+    break;
+  case 3:
+    weekDay = 'terça-feira';
+    break;
+  case 4:
+    weekDay = 'quarta-feira';
+    break;
+  case 5:
+    weekDay = 'quinta-feira';
+    break;
+  case 6:
+    weekDay = 'sexta-feira';
+    break;
+  case 7:
+    weekDay = 'sábado';
+}
 
 if (weekDay === 'segunda-feira' || 'terça-feira' || 'quarta-feira' || 'quinta-feira' || 'sexta-feira') {
   console.log('Oba, mais um dia de aprendizado na Trybe >:D');
