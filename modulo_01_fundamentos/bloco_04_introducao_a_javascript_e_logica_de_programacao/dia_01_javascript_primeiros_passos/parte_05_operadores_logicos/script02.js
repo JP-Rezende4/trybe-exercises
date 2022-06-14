71c9fcb0391575c97f472928c65bf052af4686dd
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-06-14 16:25:31 -0300

    Corrige falha no código

--- modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_05_operadores_logicos/script02.js
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_05_operadores_logicos/script02.js
@@ -27,8 +27,8 @@ switch (randomNumber) {
 
 console.log('Hoje é ' + weekDay);
 
-if (weekDay === 'segunda-feira' || 'terça-feira' || 'quarta-feira' || 'quinta-feira' || 'sexta-feira') {
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay === 'quinta-feira' || weekDay === 'sexta-feira') {
   console.log('Oba, mais um dia de aprendizado na Trybe >:D');
-} else if ('sábado' || 'domingo') {
+} else if (weekDay === 'sábado' || weekDay === 'domingo') {
   console.log('FINALMENTE, descanso merecido UwU');
 }
