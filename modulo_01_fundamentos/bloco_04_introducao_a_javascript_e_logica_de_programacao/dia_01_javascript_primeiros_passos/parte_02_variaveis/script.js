8810ef2a52944002ded768b646e6bc455c237838
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-06-14 14:12:49 -0300

    Tenta alterar o valor de birthCity e adiciona um console.log() para verificar se a alteração funcionou

--- modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_02_variaveis/script.js
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_02_variaveis/script.js
@@ -8,4 +8,9 @@ let birthYear = 2001;
 
 birthYear = 2030;
 
-console.log(birthYear);
+// console.log(birthYear);
+
+birthCity = 'Palmas';
+
+// Imprime um erro, pois não é possível reatribuir o valor de uma constante
+console.log(birthCity);
