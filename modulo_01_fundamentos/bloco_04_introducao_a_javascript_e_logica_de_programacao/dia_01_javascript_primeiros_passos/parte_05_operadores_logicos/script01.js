3c149dccc1fd7543914430ff2eaf0fa4cdfa57ce
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-06-14 15:36:01 -0300

    Cria uma estrutura condicional que imprime diferentes mensagens dependendo do valor de currentHour

--- modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_05_operadores_logicos/script01.js
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_05_operadores_logicos/script01.js
@@ -1,3 +1,15 @@
 const currentHour = 13;
 
 let message = '';
+
+if (currentHour >= 22) {
+  message = 'Não deveríamos comer nada, é hora de dormir';
+} else if (currentHour >= 18) {
+  message = 'Rango da noite, vamos jantar :D';
+} else if (currentHour >= 14) {
+  message = 'Vamos fazer um bolo pro café da tarde?';
+} else if (currentHour >= 11) {
+  message = 'Hora do almoço!!!';
+} else {
+  message = 'Hmmm, cheiro de café recém passado';
+}
