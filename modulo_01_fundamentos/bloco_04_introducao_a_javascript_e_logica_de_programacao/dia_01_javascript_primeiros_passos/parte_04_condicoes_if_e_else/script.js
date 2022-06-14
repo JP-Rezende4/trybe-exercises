181f41a225036a7a973ba8621dd4dd2a70862127
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-06-14 15:04:42 -0300

    Cria uma estrutura condicional que imprime diferentes mensagens dependendo do valor de grade

--- modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_04_condicoes_if_e_else/script.js
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_04_condicoes_if_e_else/script.js
@@ -1,1 +1,9 @@
 const grade = 80;
+
+if (grade >= 80) {
+  console.log('Parabéns, você foi aprovado(a)!');
+} else if (grade >= 60) {
+  console.log('Você está na nossa lista de espera');
+} else {
+  console.log('Você foi reprovado(a)');
+}
