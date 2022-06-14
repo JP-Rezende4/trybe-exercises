79cefad18b1cf93672739b9d751b4faafbf082b3
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-06-14 12:53:10 -0300

    Altera o valor de birthYear para 2030 e adiciona um console.log() para verificar a alteração

--- modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_02_variaveis/script.js
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_01_javascript_primeiros_passos/parte_02_variaveis/script.js
@@ -4,4 +4,8 @@ const birthCity = 'Gurupi';
 
 let birthYear = 2001;
 
-console.log(myName, birthCity, birthYear);
+// console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
