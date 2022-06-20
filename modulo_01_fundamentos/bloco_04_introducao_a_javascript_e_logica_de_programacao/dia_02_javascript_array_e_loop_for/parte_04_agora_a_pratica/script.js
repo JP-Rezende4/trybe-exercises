8a6148a395760300bbba1d509ca2ae2c96927733
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-06-20 11:54:02 -0300

    Verifica se a média é maior, menor ou igual a 20

--- modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_02_javascript_array_e_loop_for/parte_04_agora_a_pratica/script.js
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_02_javascript_array_e_loop_for/parte_04_agora_a_pratica/script.js
@@ -2,13 +2,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sumOfNumbers = 0;
 
 console.log('Requisito 1:');
-
 for (let i = 0; i < numbers.length; i += 1) {
   console.log(numbers[i]);
 }
 
 console.log('Requisito 2:');
-
 for (let i = 0; i < numbers.length; i += 1) {
   sumOfNumbers += numbers[i];
 }
@@ -16,5 +14,13 @@ for (let i = 0; i < numbers.length; i += 1) {
 console.log(sumOfNumbers);
 
 console.log('Requisito 3:');
+const average = sumOfNumbers / numbers.length;
 
-console.log(sumOfNumbers / numbers.length);
+console.log(average);
+
+console.log('Requisito 4:');
+if (average > 20) {
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}
