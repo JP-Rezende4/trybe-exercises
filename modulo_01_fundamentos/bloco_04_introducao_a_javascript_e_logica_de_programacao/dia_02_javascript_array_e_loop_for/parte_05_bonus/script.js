b366b301d9ba0325d41da08b414218e454f4bc92
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-06-20 15:38:44 -0300

    Cria um novo array a partir do array numbers obedecendo as regras do requisito

--- modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_02_javascript_array_e_loop_for/parte_05_bonus/script.js
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_02_javascript_array_e_loop_for/parte_05_bonus/script.js
@@ -29,3 +29,19 @@ for (let index = 1; index < numbers.length; index += 1) {
 for (let number of numbers) {
   console.log(number);
 }
+
+console.log('Reqquisto 3:');
+let numbers2 = [];
+numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (i === numbers.length - 1) {
+    numbers2.push(numbers[i] * 2);
+  } else {
+    numbers2.push(numbers[i] * numbers[i + 1]);
+  }
+}
+
+for (let number of numbers2) {
+  console.log(number);
+}
