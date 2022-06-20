966c3917bad73d20c48711cdfa131ebafa7cdd4e
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-06-20 11:40:34 -0300

    Soma e imprime todos os valores do array

--- modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_02_javascript_array_e_loop_for/parte_04_agora_a_pratica/script.js
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_02_javascript_array_e_loop_for/parte_04_agora_a_pratica/script.js
@@ -1,5 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sumOfNumbers = 0;
+
+console.log('Requisito 1:');
 
 for (let i = 0; i < numbers.length; i += 1) {
   console.log(numbers[i]);
 }
+
+console.log('Requisito 2:');
+
+for (let i = 0; i < numbers.length; i += 1) {
+  sumOfNumbers += numbers[i];
+}
+
+console.log(sumOfNumbers);
