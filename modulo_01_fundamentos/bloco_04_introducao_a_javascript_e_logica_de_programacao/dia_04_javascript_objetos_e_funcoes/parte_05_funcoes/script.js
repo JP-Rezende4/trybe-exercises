f58b43caf4dfa901854417b71da615dfb307c841
JP-Rezende4 <jpdr.rezende4@gmail.com>
2022-08-04 15:37:46 -0300

    Cria uma função que recebe uma string e retorne true se for um palíndromo, ou false, se não for

---
+++ modulo_01_fundamentos/bloco_04_introducao_a_javascript_e_logica_de_programacao/dia_04_javascript_objetos_e_funcoes/parte_05_funcoes/script.js
@@ -0,0 +1,20 @@
+// Exercício 1
+function isPalindrome(string) {
+  if (string === string.split('').reverse().join('')) {
+    return true;
+  }
+
+  return false;
+}
+
+// Exercício 2
+
+// Exercício 3
+
+// Exercício 4
+
+// Exercício 5
+
+// Exercício 6
+
+// Exercício 7
